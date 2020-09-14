1af4b401887e8f84ed38c9f5913c0d577ef5ee74
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-09-14 15:53:07 +0200

    Formatted BrukarUpdateRequest

--- JPATestDesign/src/main/java/BrukarUpdateRequest.java
+++ JPATestDesign/src/main/java/BrukarUpdateRequest.java
@@ -15,41 +15,42 @@ public class BrukarUpdateRequest {
         return pwd;
     }
 
-    protected BrukarUpdateRequest(){}
+    protected BrukarUpdateRequest() {
+    }
 
-    private BrukarUpdateRequest(String name, String email, String  pwd){
+    private BrukarUpdateRequest(String name, String email, String pwd) {
         this.name = name;
         this.email = email;
         this.pwd = pwd;
     }
+
     public static BrukarUpdateRequestBuilder builder() {
         return new BrukarUpdateRequestBuilder();
     }
 
-    public static class BrukarUpdateRequestBuilder{
+    public static class BrukarUpdateRequestBuilder {
         private String name;
         private String email;
         private String pwd;
 
-        public BrukarUpdateRequestBuilder name(String name){
+        public BrukarUpdateRequestBuilder name(String name) {
             this.name = name;
             return this;
         }
 
-        public BrukarUpdateRequestBuilder email(String email){
+        public BrukarUpdateRequestBuilder email(String email) {
             this.email = email;
             return this;
         }
 
-        public BrukarUpdateRequestBuilder pwd(String pwd){
+        public BrukarUpdateRequestBuilder pwd(String pwd) {
             this.pwd = pwd;
             return this;
         }
 
-        public BrukarUpdateRequest build(){
+        public BrukarUpdateRequest build() {
             return new BrukarUpdateRequest(name, email, pwd);
         }
-
     }
 
 }
